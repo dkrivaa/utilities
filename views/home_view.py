621d663c27786bd4f1e9182993b8c9de1b92eaa9
dkrivaa <dannykrivaa@hotmail.com>
2024-11-07 08:01:38 +0200

    initial commit

--- views/home_view.py
+++ views/home_view.py
@@ -11,7 +11,7 @@ with st.container(border=True):
         st.image(get_image('https://pics.freeicons.io/uploads/icons/png/15519179861536080156-64.png'))
         st.page_link('views/pdf_merge_view.py')
         st.page_link('views/pdf_split_view.py')
-        st.page_link('views/utf8_view.py')
+        st.page_link('views/pdf_from_image_view.py')
     with tab2:
         st.header('Decoding / Encoding')
         st.image(get_image('https://pics.freeicons.io/uploads/icons/png/12013790981678978252-64.png'))
