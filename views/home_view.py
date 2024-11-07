0c7a782519bb6c1d6f1ff72d852334bcce3485cc
dkrivaa <dannykrivaa@hotmail.com>
2024-11-07 08:57:30 +0200

    initial commit

--- views/home_view.py
+++ views/home_view.py
@@ -15,7 +15,7 @@ with st.container(border=True):
     with tab2:
         st.header('Decoding / Encoding')
         st.image(get_image('https://pics.freeicons.io/uploads/icons/png/12013790981678978252-64.png'))
-        st.write(f'{st.page_link('views/utf8_view.py')} - Press')
+        st.page_link('views/utf8_view.py')
     with tab3:
         st.header('Under Construction')
         st.image(get_image('https://pics.freeicons.io/uploads/icons/png/7989827331599996561-64.png'))
