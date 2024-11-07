e62b6644d5ae98d76b405c784cbe6846aab6b1c4
dkrivaa <dannykrivaa@hotmail.com>
2024-11-07 08:53:38 +0200

    initial commit

--- views/home_view.py
+++ views/home_view.py
@@ -9,23 +9,13 @@ with st.container(border=True):
     with tab1:
         st.header('PDF - Merge & Split')
         st.image(get_image('https://pics.freeicons.io/uploads/icons/png/15519179861536080156-64.png'))
-        col1, col2, col3 = st.columns([0.5, 2, 5.5], vertical_alignment='center')
-        with col1:
-            st.image(get_image('https://pics.freeicons.io/uploads/icons/png/5809905011543238858-32.png'))
-            st.image(get_image('https://pics.freeicons.io/uploads/icons/png/5809905011543238858-32.png'))
-            st.image(get_image('https://pics.freeicons.io/uploads/icons/png/5809905011543238858-32.png'))
-        with col2:
-            st.page_link('views/pdf_merge_view.py')
-            st.page_link('views/pdf_split_view.py')
-            st.page_link('views/pdf_from_image_view.py')
+        st.page_link('views/pdf_merge_view.py')
+        st.page_link('views/pdf_split_view.py')
+        st.page_link('views/pdf_from_image_view.py')
     with tab2:
         st.header('Decoding / Encoding')
         st.image(get_image('https://pics.freeicons.io/uploads/icons/png/12013790981678978252-64.png'))
-        col1, col2, col3 = st.columns([0.5, 2, 5.5], vertical_alignment='center')
-        with col1:
-            st.image(get_image('https://pics.freeicons.io/uploads/icons/png/5809905011543238858-32.png'))
-        with col2:
-            st.page_link('views/utf8_view.py')
+        st.page_link('views/utf8_view.py')
     with tab3:
         st.header('Under Construction')
         st.image(get_image('https://pics.freeicons.io/uploads/icons/png/7989827331599996561-64.png'))
